3bc4e1ed3a087c4f5b357bd4e94e97966e3b523c
Belen Cerezo <beleencere@gmail.com>
2023-04-15 06:34:37 +0200

    Initial Commit

---
+++ rickandmorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickandmorty
+//
+//  Created by Belén Cerezo on 15/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
